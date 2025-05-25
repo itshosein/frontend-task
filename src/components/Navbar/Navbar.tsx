b29bcbeba69bdbd571fdc975403d8b5ecfe1fcde
Hosein Fathi <hfathi711@gmail.com>
2025-05-25 18:25:54 +0330

    added navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <Image src="/logo.png" alt="company logo" width={38} height={38} />
+      <div className={styles.links}>
+        <div className={`${styles.home} flex-c`}>
+          <Image src="/home.svg" alt="home logo" width={24} height={24} />
+          <p>Home</p>
+        </div>
+
+        <div className="flex-c">
+          <Image src="/map.svg" alt="map logo" width={24} height={24} />
+          <p>Map</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
