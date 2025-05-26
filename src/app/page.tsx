67ee9de343a5d169d13f0d81772f76adefeaa148
Hosein Fathi <hfathi711@gmail.com>
2025-05-26 03:38:53 +0330

    added active state into navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import styles from "./page.module.css";
+import TitleDate from "@/components/TitleDate/TitleDate";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+  return <TitleDate />;
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,36 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import HomeSVG from "../svgs/Home/Home";
+import MapSVG from "../svgs/map/Map";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.navbar}>
       <Image src="/logo.svg" alt="company logo" width={38} height={38} />
       <div className={styles.links}>
-        <div className={`${styles.home} flex-c`}>
-          <Image src="/home.svg" alt="home logo" width={24} height={24} />
-          <p>Home</p>
-        </div>
+        <Link className={`${styles.home} flex-c`} href="/">
+          <HomeSVG
+            width={24}
+            height={24}
+            color={`${pathname === "/" ? "green" : "foreground"}`}
+          />
+          <p className={`${pathname === "/" ? styles.active : ""}`}>Home</p>
+        </Link>
 
-        <div className="flex-c">
-          <Image src="/map.svg" alt="map logo" width={24} height={24} />
-          <p>Map</p>
-        </div>
+        <Link className="flex-c" href="/map">
+          <MapSVG
+            width={24}
+            height={24}
+            color={`${pathname === "/map" ? "green" : "foreground"}`}
+          />
+          <p className={`${pathname === "/map" ? styles.active : ""}`}>Map</p>
+        </Link>
       </div>
     </div>
   );
