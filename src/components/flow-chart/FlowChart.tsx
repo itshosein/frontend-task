7abdd7cfd0073ce40236c6ff64e43de10c20efce
Hosein Fathi <hfathi711@gmail.com>
2025-05-27 02:47:36 +0330

    added time selector in flow chart

--- src/components/flow-chart/FlowChart.tsx
+++ src/components/flow-chart/FlowChart.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import styles from "./flow-chart.module.css";
 
 const FlowChart = () => {
-  return <div className={`flex-c ${styles.flowChart}`}>FlowChart</div>;
+  const [trailPosition, setTrailPosition] = useState(0);
+
+  return (
+    <div className={`flex-c ${styles.flowChart}`}>
+      <div className={`flex-r ${styles.titleRow}`}>
+        <p className={styles.title}>Traffic Flow Chart</p>
+        <div className={`flex-r ${styles.titleRight}`}>
+          <div className={`flex-r ${styles.timeSelector}`}>
+            <p className={styles.time} onClick={() => setTrailPosition(0)}>
+              1 day
+            </p>
+            <p className={styles.time} onClick={() => setTrailPosition(70)}>
+              1 week
+            </p>
+            <p className={styles.time} onClick={() => setTrailPosition(144)}>
+              1 hour
+            </p>
+            <div
+              className={styles.timeTrail}
+              style={{ left: `${trailPosition}px` }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FlowChart;
